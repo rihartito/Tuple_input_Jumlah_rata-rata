db420565cb27176a57ba7d72c3079467675f69a5
rihartito <78639499+rihartito@users.noreply.github.com>
2021-05-04 21:18:12 +0800

    Add files via upload

---
+++ Lab-11.py
@@ -0,0 +1,43 @@
+#Rihar del tito-71200648
+#Universitas Kristen Duta Wacana
+#Tuple
+#https://www.w3resource.com/python-exercises/tuple/
+'''
+buatlah sebuah program agar bisa menambahkan atau tidak sesuai inputan user 
+setelah itu rata-ratakan dan jumlahkan semua angka yang ada dituple,hitung ada
+berapa angka didalam Tuple tersebut,tampilkan isinya dalam tuple
+
+
+input :user = menambahkan atau tidak ; jumlah_user = angka yang dimasukkan
+
+proses :
+membuat def,mengubah menjadi list
+melakukan perulangan while 
+melakukan perulangan for 
+melakukan append 
+
+output :
+melihat tuple yang baru,rata-rata,penjumlahan,dengan jumlah isi dalam tuple
+'''
+
+def paket_lengkap(hola):
+    ubah = list(hola)
+    jumlah = 0 
+    print((ubah))
+    user = str(input("apakah anda ingin menambahkan ya/tidak :"))
+    while user == "ya" :
+        masuk = int(input("masukkan angka :"))
+        ubah.append(masuk)
+        user = str(input("apakah anda ingin menambahkan ya/tidak :"))
+    for i in ubah :
+        jumlah += i
+    print("Tuple yang baru")
+    print (tuple(ubah))
+    print("rata-ratanya adalah :",jumlah // len(ubah))
+    print("Jumlahnya adalah :",jumlah)
+    print("jumlah isi dalam tuple :",len(ubah))
+
+Tuple_baru = (2,3,4,1)
+paket_lengkap(Tuple_baru)
+
+
